9c21d1d69e83ff2bd24f1c1d1d9139070c2fb6db
DINH-THI-HONG-THAM <117697552+DINH-THI-HONG-THAM@users.noreply.github.com>
2023-05-26 14:47:42 +0700

    Update JS01.js

--- BAI03/JS01.js
+++ BAI03/JS01.js
@@ -1,10 +1,10 @@
-
 function kiemTraChanLe(so) {
-  if (so % 2 == 0) {
-    console.log("Day la so chan");
-  } else console.log("Day la so le");
+if (so % 2 === 0) {
+return "Đây là dãy số Chẵn";
+} else {
+return "Đây là dãy số  Lẻ";
 }
-
+}
 // Ví dụ sử dụng
 console.log(kiemTraChanLe(7)); // Kết quả: "Lẻ"
 console.log(kiemTraChanLe(10)); // Kết quả: "Chẵn"
